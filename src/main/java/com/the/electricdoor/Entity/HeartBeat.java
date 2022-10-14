fcddfa07ac1238480bfaecf479fa53d54341818f
stylebanana <114374221+stylebanana@users.noreply.github.com>
2022-10-14 20:48:22 +0900

    2022/10/14
    test

---
+++ src/main/java/com/the/electricdoor/Entity/HeartBeat.java
@@ -0,0 +1,18 @@
+package com.the.electricdoor.Entity;
+
+import lombok.Data;
+
+    
+@Data
+public class HeartBeat {
+    /** token*/
+    String token;
+    /** 时间戳格式:YYYYMMDDHHMMSS*/
+    String timestamp;
+    /** 旅店的uuid*/
+    String hotel;
+    /** 房间号*/
+    String room;
+    /** token+hotel+room+时间戳+密钥的MD5*/
+    String sign;
+}
---
+++ src/main/java/com/the/electricdoor/service/impl/HeartBeatServiceImpl.java
@@ -0,0 +1,25 @@
+package com.the.electricdoor.service.impl;
+
+
+
+import com.the.electricdoor.service.HeartBeatService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class HeartBeatServiceImpl implements HeartBeatService {
+    @Override
+    public String getSecretKey(String hotel, String room) {
+        return null;
+    }
+
+    @Override
+    public String md5SignatureCreate(String secretKey) {
+        return null;
+    }
+    @Override
+    public Boolean checkMd5Signature(String md5Signature) {
+        return true;
+    }
+
+}
